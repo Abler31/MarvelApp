be40059728e26b1ce713d57317f6368ce4e0b110
Abler31 <58187511+Abler31@users.noreply.github.com>
2022-02-03 18:52:57 +0300

    Добавлено отслеживание состояния загрузки списка персонажей

--- app/src/main/java/ru/my/pet/characters/FirstFragment.kt
+++ app/src/main/java/ru/my/pet/characters/FirstFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_first.view.*
 import kotlinx.coroutines.flow.collectLatest
@@ -25,7 +27,7 @@ class FirstFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
         val viewModel by viewModels<CharactersViewModel>()
-        recyclerView = view.rv_first
+        recyclerView = view.rv_characters
 
         recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
             header = CharactersLoaderStatesAdapter(adapter),
@@ -36,6 +38,16 @@ class FirstFragment : Fragment() {
             viewModel.flow.collectLatest { pagingData ->
                 adapter.submitData(pagingData)
             }
+        }
+
+        adapter.addLoadStateListener { state ->
+            recyclerView.isVisible = state.refresh is LoadState.NotLoading
+            view.progress_bar_first.isVisible = state.refresh is LoadState.Loading
+            view.button_retry_first.isVisible = state.refresh is LoadState.Error
+        }
+
+        view.button_retry_first.setOnClickListener {
+            adapter.retry()
         }
         return view
     }
