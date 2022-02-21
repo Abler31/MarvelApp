117bfed61621ff7615df8183ed230ddffd3cd28f
Abler31 <58187511+Abler31@users.noreply.github.com>
2022-02-21 13:48:59 +0300

    Добавлено появление диалога при нажатии кнопки сортировки

--- app/src/main/java/ru/my/pet/characters/FirstFragment.kt
+++ app/src/main/java/ru/my/pet/characters/FirstFragment.kt
@@ -1,5 +1,8 @@
 package ru.my.pet.characters
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -7,27 +10,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.fragment_first.view.*
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import ru.my.pet.MainActivity
 import ru.my.pet.R
 
 class FirstFragment : Fragment() {
     lateinit var recyclerView: RecyclerView
     private val adapter by lazy { FirstAdapter() }
     private val viewModel by viewModels<CharactersViewModel>()
-
     private var currentText = ""
     private val handler = Handler(Looper.getMainLooper())
+    //передача поискового вопроса
     private val searchRunnable = Runnable {
-        println(currentText)
         viewModel.submitQuery(currentText)
     }
     override fun onCreateView(
@@ -40,6 +47,7 @@ class FirstFragment : Fragment() {
         //recycler view with error/loading implementation
         recyclerView = view.rv_characters
 
+        //обработка состояния при загрузке следующей части списка
         recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
             header = CharactersLoaderStatesAdapter(adapter),
             footer = CharactersLoaderStatesAdapter(adapter)
@@ -51,6 +59,7 @@ class FirstFragment : Fragment() {
             }
         }
 
+        //реагирование на состояние изначальной загрузки списка появлением необходимых элементов
         adapter.addLoadStateListener { state ->
             recyclerView.isVisible = state.refresh is LoadState.NotLoading
             view.shimmer_first.isVisible = state.refresh is LoadState.Loading
@@ -61,15 +70,33 @@ class FirstFragment : Fragment() {
             adapter.retry()
         }
 
-        //search implementation
+        //побуквенно собираем поисковый запрос
         view.et_search.doAfterTextChanged {
             currentText = it?.toString() ?: ""
 
             handler.removeCallbacks(searchRunnable)
             handler.postDelayed(searchRunnable, 500L)
         }
+
+        //вызов кастомного диалога снизу при нажатии на кнопку сортировки
+        view.button_sort.setOnClickListener {
+            val bottomSheetDialog = context?.let { it1 ->
+                BottomSheetDialog(
+                    it1, R.style.BottomSheetDialogTheme
+                )
+            }
+            val bottomSheetView = LayoutInflater.from(context).inflate(
+                R.layout.bottom_sheet_sorting_layout,
+                view.findViewById(R.id.bottomSheet) as LinearLayout?
+            )
+            bottomSheetDialog?.setContentView(bottomSheetView)
+            bottomSheetDialog?.show()
+        }
+
         return view
     }
+
+
 
 
 }
